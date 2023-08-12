ad18fa1b2032c0893b2b4ab5fe4b6ce7a9f93001
AnnaGW <a.bogoslovskaja@yandex.ru>
2023-08-12 23:17:18 +0500

    утановила wp.

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js', // Точка входа
+  output: {
+    filename: 'bundle.[contenthash].js', // Имя бандла
+    path: path.resolve(__dirname, 'build'), // Директория для файлов сборки.
+    //resolve прелбр-т относительный путь в абсол-й, __dirname хранит абс путь к директории проекта
+    clean: true, // Удаляем предыдущую сборку перед созданием новой
+  },
+  devtool: 'source-map', // карта исходного кода, чтобы в инстр-х разраб-ка видеть не минифицированный код
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'public' }],
+    }),
+  ],
+};
