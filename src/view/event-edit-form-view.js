7924c3743fba6279ece02eba590c2b5106db8257
AnnaGW <a.bogoslovskaja@yandex.ru>
2023-08-20 22:07:09 +0500

    перенесла рендер внутр. формы во view формы (m1t3)

--- src/view/event-edit-form-view.js
+++ src/view/event-edit-form-view.js
@@ -1,8 +1,18 @@
-// форма
+// форма, все ее внутренности отрисовываются здесь
 import { createEventEditTemplate } from '../templates/event-edit-form-template';
-import { createElement } from '../render';
+import { createElement, render } from '../render';
 
+import EventEditHeaderView from '../view/event-edit-form-header-view'; // header формы
+import EventEditDetailsView from '../view/event-edit-form-details-view';
+import EventEditOffersView from '../view/event-edit-form-offers-view';
+import EventEditDestinationView from '../view/event-edit-form-destination-view';
 export default class EventEditView {
+  // insides
+  eventEditHeaderComponent = new EventEditHeaderView(); // header формы
+  eventEditDetailsComponent = new EventEditDetailsView(); // детали в форме, конт-р для офферов и пункта назна-я
+  eventEditOffersComponent = new EventEditOffersView(); // офферы в форме
+  eventEditDestinationComponent = new EventEditDestinationView(); // пункт назначения в форме
+
   getTemplate() {
     return createEventEditTemplate();
   }
@@ -12,6 +22,25 @@ export default class EventEditView {
       this.element = createElement(this.getTemplate());
     }
     return this.element;
+  }
+
+  renderInsides() {
+    render(
+      this.eventEditHeaderComponent,
+      this.getElement().querySelector('.event') // компонент вставляет внутренности в себя, header формы
+    );
+    render(
+      this.eventEditDetailsComponent,
+      this.getElement().querySelector('.event')
+    ); // детали в форме, конт-р для офферов и пункта назна-я
+    render(
+      this.eventEditOffersComponent,
+      this.eventEditDetailsComponent.getElement()
+    ); // офферы
+    render(
+      this.eventEditDestinationComponent,
+      this.eventEditDetailsComponent.getElement()
+    ); // пункт назначения
   }
 
   removeElement() {
